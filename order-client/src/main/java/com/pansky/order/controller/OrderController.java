a782db304ddb5655a61e7189dc040db908d82966
localadmin <11>
2020-02-21 11:18:48 +0800

    eureka集群的版本。 附带consul的版本。 增加了Ribbon负载均衡效果。       修改心跳时间，使用ip作为hostname

--- order-client/src/main/java/com/pansky/order/controller/OrderController.java
+++ order-client/src/main/java/com/pansky/order/controller/OrderController.java
@@ -2,15 +2,11 @@ package com.pansky.order.controller;
 
 import com.pansky.order.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 /**
  * 订单控制器
@@ -24,15 +20,9 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @GetMapping("/{id:\\d+}")
     public Object getOrder(@PathVariable Integer id) {
-        List<ServiceInstance> instances = discoveryClient.getInstances("product-server");
-        ServiceInstance serviceInstance = instances.get(0);
-        String host = serviceInstance.getHost();
-        int port = serviceInstance.getPort();
-        return restTemplate.getForObject("http://"+host + ":" + port+"/product/"+id, Product.class);
+        return restTemplate.getForObject("http://product-server/product/"+id, Product.class);
     }
 }
--- product-server/src/main/java/com/pansky/product/controller/ProductController.java
+++ product-server/src/main/java/com/pansky/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.pansky.product.controller;
 
 import com.pansky.product.entity.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("product")
 public class ProductController {
 
+    @Value("${server.port}")
+    private String port;
+    @Value("${spring.cloud.client.ip-address}")
+    private String ip;
+
     @GetMapping("/{id}")
     public Product getPrduct(@PathVariable Integer id){
         Product product = null;
         if(id == 1){
             product = new Product();
-            product.setName("测试商品");
+            product.setName("调用ip"+ip+",端口-"+port);
             product.setPrice("50元");
             product.setId(1);
         }
