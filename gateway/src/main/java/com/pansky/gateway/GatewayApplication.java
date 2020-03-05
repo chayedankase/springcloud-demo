21be19db566d791601a1c989adebcaaf7056d5b1
localadmin <11>
2020-03-05 08:40:44 +0800

    eureka集群的版本。 附带consul的版本。 增加了Ribbon负载均衡效果。 基于Feign的Sentinel熔断 基于gateway的网关       修改心跳时间，使用ip作为hostname

---
+++ gateway/src/main/java/com/pansky/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.pansky.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gateway网关启动类
+ *
+ * @author Xue-Pan
+ * @date 2020/2/28
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
